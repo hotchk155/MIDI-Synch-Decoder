6b65ff4595d083fe9b64a4ea51ad794c3713de35
Jason Hotchkiss <jason_hotchkiss@hotmail.com>
2013-04-15 22:22:21 +0100

    Version 1.00 Arpie Baseline

--- src/MidiSychInput.c
+++ src/MidiSychInput.c
@@ -1,24 +1,37 @@
 ////////////////////////////////////////////////////////////
 //
+// MIDI SYNCH RECEIVER
+//
+// Code for PIC12F1822
+// Compiled with SourceBoost C
+//
+// hotchk155/2013
+//
+// Firmware version 
+// 1.00 arpie baseline
 //
 ////////////////////////////////////////////////////////////
 
 #include <system.h>
 #include <memory.h>
 
+// 8MHz internal oscillator block, reset disabled
 #pragma DATA _CONFIG1, _FOSC_INTOSC & _WDTE_OFF & _MCLRE_OFF &_CLKOUTEN_OFF
 #pragma DATA _CONFIG2, _WRT_OFF & _PLLEN_OFF & _STVREN_ON & _BORV_19 & _LVP_OFF
 #pragma CLOCK_FREQ 8000000
 typedef unsigned char byte;
 
-
-// define the pins
-// PORTA.5 = RX
+// define the I/O pins. Note that 
+// PORTA.5 is the UART RX pin
+// PORTA.3 is left as VPP only
+#define P_LED		porta.4
 #define P_RUN		porta.0
 #define P_SYNCH		porta.1
 #define P_RESTART	porta.2
 
-#define OUTPUT_HIGH_TIME 255
+// how long the output pulses in terms of main loop cycles
+// A value of 100 gives ~2ms output pulse duration
+#define OUTPUT_HIGH_TIME 100
 
 volatile byte bRunning = 0;
 volatile byte bSynchCount = 0;
@@ -73,7 +86,7 @@ void init_usart()
 	baudcon.0 = 0;	// ABDEN	auto baud detect
 		
 	txsta.6 = 0;	// TX9		8 bit transmission
-	txsta.5 = 1;	// TXEN		transmit enable
+	txsta.5 = 0;	// TXEN		transmit enable
 	txsta.4 = 0;	// SYNC		async mode
 	txsta.3 = 0;	// SEDNB	break character
 	txsta.2 = 0;	// BRGH		high baudrate 
@@ -90,20 +103,6 @@ void init_usart()
 }
 
 ////////////////////////////////////////////////////////////
-// FLASH DIAGNOSTIC LED
-void flashLed(int i)
-{
-	while(i>0)
-	{
-		P_RUN = 1;
-		delay_ms(200);
-		P_RUN = 0;
-		delay_ms(200);
-		--i;
-	}
-}
-
-////////////////////////////////////////////////////////////
 // ENTRY POINT
 void main()
 { 
@@ -117,18 +116,29 @@ void main()
 	// configure io
 	trisa = 0b00100000;              	
 	ansela = 0b00000000;
+	porta=0;
 
 	apfcon.7=1; // RX on RA5
 	apfcon.2=1;	// TX on RA4
-	porta=0;
 
-	
+	// startup flash
+	P_LED=1; delay_ms(200);
+	P_LED=0; delay_ms(200);
+	P_LED=1; delay_ms(200);
+	P_LED=0; delay_ms(200);
+	P_LED=1; delay_ms(200);
+	P_LED=0; delay_ms(200);
+
+	// initialise USART
 	init_usart();
-	flashLed(3);
 
-	// loop forever
+	// loop forever		
+	unsigned long count = 0;
 	for(;;)
 	{
+		P_LED = ((count & 0xf000UL) == 0x1000UL);
+		++count;
+			
 		if(bSynchCount)
 		{
 			if(!--bSynchCount) 
@@ -141,6 +151,3 @@ void main()
 		}		
 	}
 }
-
-
-
